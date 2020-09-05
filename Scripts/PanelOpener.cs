e89699b149ee3bc7243825a532f3b1d2da6fdd33
archana-17 <lcs2019041@iiitl.ac.in>
2020-09-05 18:47:39 +0530

    Added script tot bo back to choice menu from Alphabet scene

---
+++ Scripts/PanelOpener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+    public void openPanel(){
+        if(Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
