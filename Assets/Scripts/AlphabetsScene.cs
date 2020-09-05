2360b316a1bcf8f6635861ae7a4de36d14c0548b
Sukriti Dawar <sukritidawar@gmail.com>
2020-09-05 19:10:38 +0530

    Added Assets folder

---
+++ Assets/Scripts/AlphabetsScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AlphabetsScene : MonoBehaviour
+{
+    public void PlayGame () 
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/NumbersScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NumbersScene : MonoBehaviour
+{
+    public void PlayGame () 
+    {
+        SceneManager.LoadScene(3);
+    }
+}
